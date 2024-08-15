5d4053d8e6094bca959ca97dff3fd12aff06c846
jrTilak <tilakthapa9898@gmail.com>
2024-08-15 15:41:23 +0545

    Revert "build"
    This reverts commit 7633a1639d2cbf55dc07f1edbffc3c9648100a56.

--- server/dist/constants/time.const.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OPT_EXPIRATION = void 0;
-exports.OPT_EXPIRATION = 2 * 60;
-//# sourceMappingURL=time.const.js.map
