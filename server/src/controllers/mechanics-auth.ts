d508ce07fd94df2bb63eed04310d1299944e8e19
jrTilak <tilakthapa9898@gmail.com>
2024-09-11 22:31:07 +0545

    feat: add all auth api to mechanics

--- server/src/controllers/mechanics-auth.ts
+++ server/src/controllers/mechanics-auth.ts
@@ -1,13 +1,13 @@
 import { sendRes } from "@/middlewares/send-response";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import { generateOTP } from "@/utils/generate-otp";
 import { clearCookies, registerCookies } from "@/utils/cookies";
 import ENV_CONFIG from "@/config/env.config";
 import mechanicsModel from "@/models/mechanics.model";
 import { IMechanicsLoginSchema, IMechanicsSignupSchema } from "@/zod";
-import appointmentModel from "@/models/appointment.model";
-import serviceModel from "@/models/service.model";
+import OTPModel from "@/models/otp.model";
+import { generateOTP, validateOTP } from "@/utils/generate-otp";
+import { otpMailer } from "@/mailers/otp-mailer";
 
 export const handleMechanicsSignup = async (
   req: Request<{}, {}, IMechanicsSignupSchema>,
@@ -100,5 +100,246 @@ export const handleMechanicsLogout = async (req: Request, res: Response) => {
   return sendRes(res, {
     status: 200,
     message: "Logged out successfully!",
+  });
+};
+
+export const handleVerifyOTPForMechanicsSignup = async (
+  req: Request,
+  res: Response
+) => {
+  const { otp: userOtp } = req.body;
+
+  if (!userOtp) {
+    return sendRes(res, {
+      status: 422,
+      message: "Please provide OTP!",
+    });
+  }
+
+  const user = res.locals.jwtData.userId;
+
+  const otpRecord = await OTPModel.findOne({
+    userId: user,
+    purpose: "ACCOUNT_VERIFICATION",
+    role: "MECHANIC",
+  });
+
+  if (!otpRecord) {
+    return sendRes(res, {
+      status: 400,
+      message: "OTP is expired, Please request a new one!",
+    });
+  }
+
+  const { isValid, reason } = validateOTP({
+    existingOTP: otpRecord.otp,
+    expiresAt: otpRecord.expiresAt,
+    otp: userOtp,
+  });
+
+  if (!isValid) {
+    const errorMessage =
+      reason === "EXPIRED"
+        ? "OTP is expired, Please request a new one!"
+        : "Invalid OTP, Please try again";
+    return sendRes(res, {
+      status: 400,
+      message: errorMessage,
+    });
+  }
+
+  await otpRecord.deleteOne();
+  await mechanicsModel.findByIdAndUpdate(user, { isVerified: true });
+
+  return sendRes(res, {
+    status: 200,
+    message: "You account is verified!",
+  });
+};
+
+export const handleResendOTPForMechanicsSignup = async (
+  req: Request,
+  res: Response
+) => {
+  const user = res.locals.jwtData.userId;
+
+  const userDoc = await mechanicsModel.findById(user);
+  if (!userDoc) {
+    return sendRes(res, {
+      status: 404,
+      message: "User not found!",
+    });
+  }
+
+  if (userDoc.isVerified) {
+    return sendRes(res, {
+      status: 400,
+      message: "Your account is already verified!",
+    });
+  }
+
+  // this is because we want to delete the previous otp
+  await OTPModel.findOneAndDelete({
+    userId: user,
+    purpose: "ACCOUNT_VERIFICATION",
+    role: "MECHANIC",
+  });
+
+  const otp = generateOTP();
+
+  await OTPModel.create({
+    userId: user,
+    role: "MECHANIC",
+    otp: parseInt(otp),
+    purpose: "ACCOUNT_VERIFICATION",
+  });
+
+  otpMailer(otp, userDoc.email);
+
+  return sendRes(res, {
+    status: 200,
+    message: "OTP sent to your email!",
+  });
+};
+
+export const handleResetMechanicsPassword = async (
+  req: Request,
+  res: Response
+) => {
+  const { email } = req.body;
+  const user = await mechanicsModel.findOne({ email });
+  if (!user) {
+    return sendRes(res, {
+      status: 404,
+      message: "User not found!",
+    });
+  }
+
+  await OTPModel.findOneAndDelete({
+    userId: user._id,
+    purpose: "RESET_PASSWORD",
+    role: "MECHANIC",
+  });
+
+  const otp = generateOTP();
+
+  await OTPModel.create({
+    userId: user._id,
+    role: "MECHANIC",
+    otp: parseInt(otp),
+    purpose: "RESET_PASSWORD",
+  });
+
+  otpMailer(otp, user.email);
+
+  return sendRes(res, {
+    status: 200,
+    message: "OTP sent to your email!",
+  });
+};
+
+export const handleVerifyOTPForMechanicsResetPassword = async (
+  req: Request,
+  res: Response
+) => {
+  const { otp: userOtp, email, newPassword } = req.body;
+
+  if (!userOtp) {
+    return sendRes(res, {
+      status: 422,
+      message: "Please provide OTP!",
+    });
+  }
+
+  const user = await mechanicsModel.findOne({ email });
+
+  if (!user) {
+    return sendRes(res, {
+      status: 404,
+      message: "User not found!",
+    });
+  }
+
+  const otpRecord = await OTPModel.findOne({
+    userId: user._id,
+    purpose: "RESET_PASSWORD",
+    role: "MECHANIC",
+  });
+
+  if (!otpRecord) {
+    return sendRes(res, {
+      status: 400,
+      message: "OTP is expired, Please request a new one!",
+    });
+  }
+  console.log(otpRecord.otp, userOtp);
+
+  const { isValid, reason } = validateOTP({
+    existingOTP: otpRecord.otp,
+    expiresAt: otpRecord.expiresAt,
+    otp: userOtp,
+  });
+
+  if (!isValid) {
+    const errorMessage =
+      reason === "EXPIRED"
+        ? "OTP is expired, Please request a new one!"
+        : "Invalid OTP, Please try again";
+    return sendRes(res, {
+      status: 400,
+      message: errorMessage,
+    });
+  }
+
+  await otpRecord.deleteOne();
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+  await mechanicsModel.findByIdAndUpdate(user, { password: hashedPassword });
+
+  return sendRes(res, {
+    status: 200,
+    message: "You password is updated, you can login now!",
+  });
+};
+
+export const handleChangeMechanicsPassword = async (
+  req: Request,
+  res: Response
+) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (oldPassword === newPassword) {
+    return sendRes(res, {
+      status: 400,
+      message: "Old password and new password can't be same!",
+    });
+  }
+
+  const user = res.locals.jwtData.userId;
+  const userDoc = await mechanicsModel.findById(user);
+  if (!userDoc) {
+    return sendRes(res, {
+      status: 404,
+      message: "User not found!",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(oldPassword, userDoc.password);
+
+  if (!isPasswordValid) {
+    return sendRes(res, {
+      status: 400,
+      message: "Invalid old password!",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+  await mechanicsModel.findByIdAndUpdate(user, { password: hashedPassword });
+
+  return sendRes(res, {
+    status: 200,
+    message: "You password is updated!",
   });
 };
--- server/src/routes/mechanics-auth.ts
+++ server/src/routes/mechanics-auth.ts
@@ -3,13 +3,23 @@ import {
   handleGetMechanicsProfile,
   handleMechanicsLogout,
   handleMechanicsLogin,
+  handleVerifyOTPForMechanicsSignup,
+  handleResendOTPForMechanicsSignup,
+  handleResetMechanicsPassword,
+  handleChangeMechanicsPassword,
+  handleVerifyOTPForMechanicsResetPassword,
 } from "@/controllers/mechanics-auth";
 import { validateBody } from "@/middlewares/validate-body";
 import { Router } from "express";
 import { verifyToken } from "@/utils/token-manager";
 import ENV_CONFIG from "@/config/env.config";
-import { MechanicsLoginSchema, MechanicsSignupSchema } from "@/zod";
+import {
+  MechanicsLoginSchema,
+  MechanicsSignupSchema,
+  ResetPasswordSchema,
+} from "@/zod";
 import { tryCatch } from "@/middlewares/try-catch";
+import z from "zod";
 
 const authRouter = Router();
 
@@ -23,6 +33,45 @@ authRouter.post(
   validateBody(MechanicsLoginSchema),
   tryCatch(handleMechanicsLogin)
 );
+
+authRouter.post(
+  "/verify",
+  verifyToken(ENV_CONFIG.MECHANICS_AUTH_TOKEN_ID),
+  tryCatch(handleVerifyOTPForMechanicsSignup)
+);
+
+authRouter.get(
+  "/signup/resend-otp",
+  verifyToken(ENV_CONFIG.MECHANICS_AUTH_TOKEN_ID),
+  tryCatch(handleResendOTPForMechanicsSignup)
+);
+
+authRouter.post(
+  "/reset-password",
+  validateBody(
+    z.object({ email: z.string().email("Please provide a valid email") })
+  ),
+  tryCatch(handleResetMechanicsPassword)
+);
+
+authRouter.put(
+  "/update-password",
+  verifyToken(ENV_CONFIG.MECHANICS_AUTH_TOKEN_ID),
+  validateBody(
+    z.object({
+      oldPassword: z.string(),
+      newPassword: z.string().min(6),
+    })
+  ),
+  tryCatch(handleChangeMechanicsPassword)
+);
+
+authRouter.post(
+  "/reset-password/verify",
+  validateBody(ResetPasswordSchema),
+  tryCatch(handleVerifyOTPForMechanicsResetPassword)
+);
+
 authRouter.get(
   "/me",
   verifyToken(ENV_CONFIG.MECHANICS_AUTH_TOKEN_ID),
