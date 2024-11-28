d0464cb3c61a46d7d3313b5d8090e1a70cb4fc32
nabinkhair42 <nabinkhair12@gmail.com>
2024-11-29 00:48:04 +0545

    Update admin API URLs for appointment requests and add token validation endpoint

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -58,8 +58,10 @@ export const API_URL = {
   GET_DASHBOARD_STATS: BASE_URL + "/admin/stats",
   GET_MECHANICS_LISTS: BASE_URL + "/admin/mechanics",
   GET_APPOINTMENT_REQUESTS: BASE_URL + "/admin/appointments",
-  APPROVE_APPOINTMENT_REQUEST: BASE_URL + "/admin/appointments/approve",
-  REJECT_APPOINTMENT_REQUEST: BASE_URL + "/admin/appointments/reject",
+  APPROVE_APPOINTMENT_REQUEST: BASE_URL + "/admin/appointments",
+  REJECT_APPOINTMENT_REQUEST: BASE_URL + "/admin/appointments",
+
+  VALIDATE_TOKEN : BASE_URL + "/admin/token",
 };
 
 export const adminLoginToken = process.env.NEXT_PUBLIC_ADMIN_LOGIN_TOKEN;
